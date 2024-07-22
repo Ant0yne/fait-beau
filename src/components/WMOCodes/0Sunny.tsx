ec564c78acad6a00b1b3d617c99ec55b646fec5b
Ant0yne <antoine.soliv@gmail.com>
2024-07-22 19:09:21 +0200

    first test animation sunny component

--- src/components/WMOCodes/0Sunny.tsx
+++ src/components/WMOCodes/0Sunny.tsx
@@ -1,15 +1,28 @@
+import { motion } from "framer-motion";
+
 import Image from "next/image";
 
 const Sunny = () => {
 	return (
-		<div>
+		<div className="flex flex-col gap-10">
 			<h2 className="text-center text-3xl font-bold">Sunny</h2>
-			<Image
-				src="https://raw.githubusercontent.com/roe-dl/weathericons/db4f99ea682e252f5bf0dac118f7bab9aea3450b/weathericons-filled/clear-day.svg"
-				width={500}
-				height={500}
-				alt="A sun animation"
-			/>
+			<motion.div
+				animate={{
+					rotate: [0, 45, 90, 135, 180, 225, 270, 315],
+					scale: [1, 1.1, 1, 1.1, 1.1, 1, 1.1, 1],
+				}}
+				transition={{
+					repeat: Infinity,
+					duration: 10,
+					ease: "linear",
+				}}>
+				<Image
+					src="https://raw.githubusercontent.com/roe-dl/weathericons/db4f99ea682e252f5bf0dac118f7bab9aea3450b/weathericons-filled/clear-day.svg"
+					width={500}
+					height={500}
+					alt="A sun animation"
+				/>
+			</motion.div>
 		</div>
 	);
 };
